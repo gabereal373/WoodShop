c02ba932035582189eaefeb7916752eab78cfbe8
Jeffrey Forte <jforte142@gmail.com>
2020-12-15 00:15:03 -0500

    app.js --> pretty sure the code functions correctly. just need to configure mondodb to take remote connections

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header");
+const {MongoClient} = require("mongodb");        //create client class
 
 let prevScrollPos = window.pageYOffset;
 
@@ -11,4 +12,21 @@ window.onscroll = function () {
   prevScrollPos = currentScrollPos;
 };
 
+const uri = "mongodb://127.0.0.1:27017"
 
+async function connect() {
+    const client = new MongoClient(uri)
+    try {
+        await client.connect();
+        const db = client.db("woodshop");
+        console.log('Connected to database')
+    }
+    catch (ex) {
+        console.error('Something bad happened')
+
+    }
+    finally {
+        client.close();
+    }
+}
+connect();
