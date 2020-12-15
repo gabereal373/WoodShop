9ec5f9ef049a3a93cb0979773a77a85756eec142
gabereal373 <gabereal373@gmail.com>
2020-12-15 03:07:11 -0500

    commit to 'master'

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const header = document.querySelector("header");
 const {MongoClient} = require("mongodb");        //create client class
 
-let prevScrollPos = window.pageYOffset;
-
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  prevScrollPos > currentScrollPos 
-    ? header.classList.remove("scroll") 
-    : header.classList.add("scroll");
-
-  prevScrollPos = currentScrollPos;
-};
 
 const uri = "mongodb://127.0.0.1:27017"
 
