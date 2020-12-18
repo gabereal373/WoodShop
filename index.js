fcf5b0d31319b9ee552c3b162330c7cfde6ee65e
Jeffrey Forte <jforte142@gmail.com>
2020-12-17 19:27:50 -0500

    Form Submission/Email Send Complete!

--- index.js
+++
@@ -1,9 +0,0 @@
-//Basic HTML Server Setup
-var static = require('node-static');
-var http = require('http');
-
-var file = new(static.Server)();
-
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(8080);
