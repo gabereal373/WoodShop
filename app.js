8689da71a6b1b0a84de2ae71426749308dd0f699
jforte1495 <jforte142@gmail.com>
2020-12-21 22:27:05 -0500

    just trying something

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-const { MongoClient } = require("mongodb")      //create client class
-const uri = "mongodb://10.0.0.208:27017"
+var express = require('express');
+var path = require('path');
+var mongodb = require('mongodb');
+var bodyParser = require('body-parser');
+
+
+var dbb = mongodb.MongoClient.connect('mongodb://localhost:27017');                  //connects to database
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, 'public')));                         //serve static content
+
+
+//handler for Form Post
+app.post('/contact-us', function (req, res) {
+    dbb.then(function(db) {
+        delete req.body._id; // for safety reasons
+        db.collection('woodshop').insertOne(req.body);
+    });    
+    res.send('Data received:\n' + JSON.stringify(req.body));
+});
+
+//handler for data view
+app.get('/view-feedbacks',  function(req, res) {
+    dbb.then(function(db) {
+        db.collection('woodshop').find({}).toArray().then(function(feedbacks) {
+            res.status(200).json(feedbacks);
+        });
+    });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
 
 
 
 
-async function contactUs() {
-    const client = new MongoClient(uri);
-    try {
-        await client.connect();
-        const db = client.db("woodshop");
-        console.log(`Connected to database ${db.databaseName}`);
-        var query = {"name" : "rocky"};
-        // var query = {"name" : name, "email" : email, "address" : address, "message" : message};
-        db.collection("contactUs").insertOne(query);
-        console.log("Insertion Complete");
-    }
-    catch (ex) {
-        console.error(`Something bad happened ${ex}`);
 
-    }
-    finally {
-        client.close();
-    }
 
-};
-contactUs();
+g
+
+
+
+
+
+
+
+
+
 
